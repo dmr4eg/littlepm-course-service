9cf4c85b616866d280c11e79de2237d01c91247e
Damir @dmr4eg <fifth.dmr@gmail.com>
2025-02-15 23:46:52 +0100

    :rocket: - Init project

---
+++ proj-conf-service/src/main/java/pm/little/projconfservice/ProjConfServiceApplication.java
@@ -0,0 +1,13 @@
+package pm.little.projconfservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjConfServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjConfServiceApplication.class, args);
+    }
+
+}
---
+++ proj-conf-service/src/test/java/pm/little/projconfservice/ProjConfServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pm.little.projconfservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjConfServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
